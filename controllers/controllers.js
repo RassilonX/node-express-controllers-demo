7181029818af5c3454d3ec5d75bc7c5008326d47
RassilonX <chrisfraser18@gmail.com>
2020-10-02 14:59:56 +0100

    Example

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -4,5 +4,15 @@ var ObjectId = require("mongodb").ObjectId;
 
 module.exports = {
     
+    viewAll: function(app, req, res){
+        console.info('Helo from the controller')
+        app.set('myDb').collection('filmsCollection').find({}).toArray(function(err, docs){
+            console.dir(docs)
+            return res.render('allFilms', {
+                title: 'All Films',
+                films: docs
+            })
+        })
+    }
 
 }
